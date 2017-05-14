97ba932788b4950e1c9dc55940302b031a7335b0
pv42 <pv42.97@gmail.com>
2017-05-14 12:44:32 +0200

    comment

--- src/engine/toolbox/Conversion.java
+++ src/engine/toolbox/Conversion.java
@@ -10,6 +10,7 @@ import org.lwjgl.util.vector.Vector4f;
  * Created by pv42 on 21.07.16.
  */
 public class Conversion {
+    private Conversion() {} //this can never be an object
     public static Vector2f pixelFromOpenGLSpace2D(Vector2f openGLSpace) {
         Vector2f size = DisplayManager.getSize();
         return new Vector2f((openGLSpace.x + 1)*.5f* size.x,(openGLSpace.y + 1)*.5f* size.y);
