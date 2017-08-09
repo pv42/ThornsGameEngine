65b6e052e4965738e673f130c292aff13b7ee7f9
pv42 <pv42.97@gmail.com>
2017-08-09 17:40:14 +0200

    refractoring

--- src/engine/graphics/shaders/StaticShader.java
+++ src/engine/graphics/shaders/StaticShader.java
@@ -15,7 +15,7 @@ import java.util.List;
 /***
  * Created by pv42 on 17.06.16.
  */
-public class StaticShader extends ShaderProgram{
+public class StaticShader extends ShaderProgram {
     private static final String VERTEX_FILE = "src/engine/graphics/shaders/glsl/vertexShader";
     private static final String FRAGMENT_FILE = "src/engine/graphics/shaders/glsl/fragmentShader";
     private static final int MAX_LIGHTS = 4;
@@ -37,12 +37,14 @@ public class StaticShader extends ShaderProgram{
     private int location_ambientLight;
     private int locations_bone[];
     private int location_useAnimation;
+
     public StaticShader() {
-        super(VERTEX_FILE,FRAGMENT_FILE);
+        super(VERTEX_FILE, FRAGMENT_FILE);
         start();
         loadAmbientLight(Settings.AMBIENT_LIGHT);
         stop();
     }
+
     @Override
     protected void bindAttributes() {
         super.bindAttribute(0, "position");
@@ -61,8 +63,8 @@ public class StaticShader extends ShaderProgram{
         locations_lightPosition = new int[MAX_LIGHTS];
         locations_lightColor = new int[MAX_LIGHTS];
         locations_attenuation = new int[MAX_LIGHTS];
-        for (int i = 0; i<4;i++) {
-            locations_lightPosition[i] = super.getUniformLocation("lightPosition[" + i + "]" );
+        for (int i = 0; i < 4; i++) {
+            locations_lightPosition[i] = super.getUniformLocation("lightPosition[" + i + "]");
             locations_lightColor[i] = super.getUniformLocation("lightColor[" + i + "]");
             locations_attenuation[i] = super.getUniformLocation("attenuation[" + i + "]");
         }
@@ -77,70 +79,84 @@ public class StaticShader extends ShaderProgram{
         location_texture = super.getUniformLocation("diffTexture");
         location_ambientLight = super.getUniformLocation("ambient_light");
         locations_bone = new int[Settings.MAX_BONES];
-        for (int i = 0; i < Settings.MAX_BONES; i++){
+        for (int i = 0; i < Settings.MAX_BONES; i++) {
             locations_bone[i] = super.getUniformLocation("bone[" + i + "]");
         }
         location_useAnimation = super.getUniformLocation("useAnimation");
     }
+
     public void connectTextures() {
-        super.loadInt(location_texture,0);
-        super.loadInt(location_specMap,1);
+        super.loadInt(location_texture, 0);
+        super.loadInt(location_specMap, 1);
     }
+
     public void loadTransformationMatrix(Matrix4f matrix) {
-        super.loadMatrix(location_transformationMatrix,matrix);
+        super.loadMatrix(location_transformationMatrix, matrix);
     }
+
     public void loadProjectionMatrix(Matrix4f matrix) {
-        super.loadMatrix(location_projectionMatrix,matrix);
+        super.loadMatrix(location_projectionMatrix, matrix);
     }
+
     public void loadViewMatrix(Camera camera) {
         Matrix4f viewMatrix = Maths.createViewMatrix(camera);
-        super.loadMatrix(location_viewMatrix,viewMatrix);
+        super.loadMatrix(location_viewMatrix, viewMatrix);
     }
+
     public void loadLights(List<Light> lights) {
-        for (int i =0; i< MAX_LIGHTS; i++) {
+        for (int i = 0; i < MAX_LIGHTS; i++) {
             if (i < lights.size()) {
                 super.loadVector(locations_lightPosition[i], lights.get(i).getPosition());
                 super.loadVector(locations_lightColor[i], lights.get(i).getColor().getVector());
                 super.loadVector(locations_attenuation[i], lights.get(i).getAttenuation());
-            } else  {
-                super.loadVector(locations_lightPosition[i], new Vector3f(0,0,0));
-                super.loadVector(locations_lightColor[i], new Vector3f(0,0,0));
-                super.loadVector(locations_attenuation[i], new Vector3f(1,0,0));
+            } else {
+                super.loadVector(locations_lightPosition[i], new Vector3f(0, 0, 0));
+                super.loadVector(locations_lightColor[i], new Vector3f(0, 0, 0));
+                super.loadVector(locations_attenuation[i], new Vector3f(1, 0, 0));
             }
         }
     }
+
     public void loadShineVariables(float shineDamper, float reflectivity) {
-        super.loadFloat(location_shineDamper,shineDamper);
-        super.loadFloat(location_reflectivity,reflectivity);
+        super.loadFloat(location_shineDamper, shineDamper);
+        super.loadFloat(location_reflectivity, reflectivity);
     }
+
     public void loadFakeLightning(boolean useFakeLightning) {
         super.loadBoolean(location_useFakeLightning, useFakeLightning);
     }
+
     public void loadUseSpecMap(boolean usesSpecMap) {
         super.loadBoolean(location_usesSpecMap, usesSpecMap);
     }
+
     public void loadSkyColor(Color skyColor) {
         super.loadVector(location_skyColor, skyColor.getVector());
     }
+
     public void loadNumberOfRows(int numberOfRows) {
-        super.loadFloat(location_numberOfRows,numberOfRows);
+        super.loadFloat(location_numberOfRows, numberOfRows);
     }
+
     public void loadOffset(float offsetX, float offsetY) {
-        super.loadVector(location_offset,new Vector2f(offsetX,offsetY));
+        super.loadVector(location_offset, new Vector2f(offsetX, offsetY));
     }
+
     public void loadAmbientLight(float ambient) {
-        super.loadFloat(location_ambientLight,ambient);
+        super.loadFloat(location_ambientLight, ambient);
     }
-    public void loadBones (List<Matrix4f> bones) {
-        for(int i = 0; i < Settings.MAX_BONES; i++) {
+
+    public void loadBones(List<Matrix4f> bones) {
+        for (int i = 0; i < Settings.MAX_BONES; i++) {
             if (i < bones.size()) {
                 super.loadMatrix(locations_bone[i], bones.get(i));
-            } else  {
+            } else {
                 super.loadMatrix(locations_bone[i], new Matrix4f());
             }
         }
     }
+
     public void loadUseAnimation(boolean useAnimation) {
-        super.loadBoolean(location_useAnimation,true);
+        super.loadBoolean(location_useAnimation, true);
     }
 }
