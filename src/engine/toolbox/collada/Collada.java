591ba2a61f6a7786f47a9eda4e3a3228c1e73712
pv42 <pv42.97@gmail.com>
2017-11-05 00:52:20 +0100

    added collada interface

--- src/engine/toolbox/collada/Collada.java
+++ src/engine/toolbox/collada/Collada.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
-public class Collada {
+public class Collada implements ICollada{
     private ColladaAsset colladaAsset;
     private Map<String, ColladaImage> images;
     private Map<String, ColladaMaterial> materials;
